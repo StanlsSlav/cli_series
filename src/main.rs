b10c8b3bca50d1d8ee6b2341b3c9db343ac116f4
StanlsSlav <65411828+StanlsSlav@users.noreply.github.com>
2024-06-30 14:32:45 +0200

    feat: add help banner and message
    Also refactor some import qualifications

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ mod term;
 use std::{error::Error, io::stdout, time::Duration};
 
 use crossterm::{
-    event::{poll, read, KeyEventKind},
+    event::{poll, read, Event, KeyCode, KeyEvent, KeyEventKind},
     execute,
-    style::{Attribute, Print, ResetColor, SetAttribute},
+    style::{Attribute, Color, Print, ResetColor, SetAttribute, SetForegroundColor},
 };
 use printer::print_series_table;
 use series::Series;
@@ -17,6 +17,8 @@ use term::clear_screen;
 struct App {
     should_render: bool,
     should_exit: bool,
+    should_show_help: bool,
+    should_show_help_msg: bool,
     data: Data,
 }
 
@@ -32,6 +34,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut app = App {
         should_render: true,
         should_exit: false,
+        should_show_help: false,
+        should_show_help_msg: true,
         data: Data {
             hovered_serie_idx: Some(0),
             available_series: Vec::new(),
@@ -60,8 +64,36 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn print_help() -> Result<(), Box<dyn Error>> {
+    let keybinds = [
+        ('h', "Toggle [h]elp"),
+        ('m', "Toggle help [m]essage"),
+        ('r', "Force [r]efresh the series"),
+        ('k', "Move up"),
+        ('j', "Move down"),
+        ('G', "Move to last serie"),
+    ];
+
+    for (key, desc) in keybinds.iter() {
+        execute!(
+            stdout(),
+            SetForegroundColor(Color::Magenta),
+            Print(key),
+            ResetColor,
+            Print(format!(": {}\n", desc)),
+        )?;
+    }
+
+    Ok(())
+}
+
 fn render(app: &mut App) -> Result<(), Box<dyn Error>> {
     clear_screen();
+
+    if app.should_show_help {
+        print_help()?;
+        return Ok(());
+    }
 
     let data = &mut app.data;
 
@@ -73,33 +105,32 @@ fn render(app: &mut App) -> Result<(), Box<dyn Error>> {
     }
 
     print_series_table(&data.available_series, data.hovered_serie_idx)?;
+
+    if app.should_show_help_msg {
+        println!("\nPress [h] for keybinds");
+    }
+
     Ok(())
 }
 
 fn handle_input(app: &mut App) -> Result<(), Box<dyn Error>> {
     if poll(Duration::from_millis(100))? {
         match read()? {
-            crossterm::event::Event::FocusGained
-            | crossterm::event::Event::FocusLost
-            | crossterm::event::Event::Mouse(_)
-            | crossterm::event::Event::Paste(_) => (),
-            crossterm::event::Event::Resize(_, _) => app.should_render = true,
-            crossterm::event::Event::Key(key_ev) => handle_keyboard_input(app, key_ev)?,
+            Event::FocusGained | Event::FocusLost | Event::Mouse(_) | Event::Paste(_) => (),
+            Event::Resize(_, _) => app.should_render = true,
+            Event::Key(key_ev) => handle_keyboard_input(app, key_ev)?,
         }
     }
 
     Ok(())
 }
 
-fn handle_keyboard_input(
-    app: &mut App,
-    event: crossterm::event::KeyEvent,
-) -> Result<(), Box<dyn Error>> {
+fn handle_keyboard_input(app: &mut App, event: KeyEvent) -> Result<(), Box<dyn Error>> {
     if event.kind != KeyEventKind::Press {
         return Ok(());
     }
 
-    if let crossterm::event::KeyCode::Char(chr) = event.code {
+    if let KeyCode::Char(chr) = event.code {
         app.should_render = true;
 
         match chr {
@@ -107,7 +138,13 @@ fn handle_keyboard_input(
                 app.should_exit = true;
             }
 
-            'r' | 'u' => app.data.ignore_cached_series = true,
+            'h' => {
+                app.should_show_help = !app.should_show_help;
+            }
+
+            'm' => app.should_show_help_msg = !app.should_show_help_msg,
+
+            'r' => app.data.ignore_cached_series = true,
 
             'k' => {
                 if let Some(idx) = app.data.hovered_serie_idx {
